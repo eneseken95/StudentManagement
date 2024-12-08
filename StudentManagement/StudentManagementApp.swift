a2a98f3a3ecbdace4dfa3c525b7bc785272b354c
Enes Eken <144843964+eneseken95@users.noreply.github.com>
2024-12-08 13:25:11 +0300

    Initial commit

---
+++ StudentManagement/StudentManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentManagementApp.swift
+//  StudentManagement
+//
+//  Created by Enes Eken 2 on 8.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct StudentManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
